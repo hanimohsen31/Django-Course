b57a912d1b08e6b22603af2272babfec55f69333
hanimohsen3810 <hanimohsen3810@yahoo.com.com>
2021-11-11 23:56:44 +0200

    Customizing Job v1

--- job/models.py
+++ job/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+JOB_TITLE = (
+    ('Full Time','Full Time'),
+    ('Part Time', 'Part Time'),
+)
+
+
+class Job(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, default='')
+    # location = ""
+    job_type = models.CharField(max_length=15, choices=JOB_TITLE)
+    description = models.TextField(max_length=1000, default='')
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    experience = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='job/',default='')
+
+    def __str__(self):
+        return self.title
+
+
--- job/views.py
+++ job/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Job
+
 
 # Create your views here.
+def joblist(request):
+    joblist1 = Job.objects.all()
+    context = {'joblist1': joblist1}
+    return render(request, 'job/joblist.html', context)
+
+
+def jobdetails(request, id):
+    jobdetail = Job.objects.get(id=id)
+    context = {'jobdetail': jobdetail}
+    return render(request, 'job/jobdetails.html', context)
